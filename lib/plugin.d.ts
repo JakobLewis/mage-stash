ef5b4544cf66b7f59af28aa4ba339c6605ed7a6f
= <=>
2023-01-28 20:43:17 +0100

    added runtime loading, removing and exit cleanup of generic Plugin objects

--- lib/plugin.d.ts
+++ lib/plugin.d.ts
@@ -1,11 +1,9 @@
 export interface Plugin {
-    /** The unique display name of this plugin */
     readonly name: string;
-    /** Lifecycle hooks */
     readonly hooks: {
-        /** Triggered before the plugin is loaded */
         readonly start?: () => void;
-        /** Triggered after the plugin is unloaded */
         readonly stop?: () => void;
     };
 }
+export declare function load(plugin: Plugin): boolean;
+export declare function remove(plugin: Plugin): boolean;
--- src/plugin.ts
+++ src/plugin.ts
@@ -1,31 +1,81 @@
-
-// TODO: Solidify search behavior
-// TODO: Implement partial/scoped search
-
-/* TODO: Migrate common code from library.ts & manifest.ts here instead
-export interface Quartermaster<T extends Plugin> {
-    readonly domainName: string;
-    readonly ownedPlugins: T[];
-
-    readonly hooks: {
-        readonly start: () => void;
-        // Can reject plugins
-        readonly loadingPlugin: (plugin: any) => plugin is T;
-        readonly removingPlugin: (plugin: T) => void;
-        readonly stop: () => void;
-    }
-}
-*/
+import Logger from "./logging";
 
 export interface Plugin {
-    /** The unique display name of this plugin */
     readonly name: string;
 
-    /** Lifecycle hooks */
     readonly hooks: {
-        /** Triggered before the plugin is loaded */
         readonly start?: () => void;
-        /** Triggered after the plugin is unloaded */
         readonly stop?: () => void;
     }
 }
+
+const logger = new Logger('plugin.js');
+
+const Plugins = new Array<Plugin>();
+const pluginNames = new Set<string>();
+
+export function load(plugin: Plugin): boolean {
+    if (Plugins.includes(plugin)) {
+        logger.warn(`Plugin<${plugin.name}> cannot be loaded more than once`);
+        return false;
+    }
+
+    if (pluginNames.has(plugin.name)) {
+        logger.warn(`Plugin<${plugin.name}> cannot be loaded because another plugin with the same name is already loaded`);
+        return false;
+    }
+
+    if (plugin.hooks.start) {
+        try {
+            plugin.hooks.start();
+        }
+        catch (e) {
+            logger.descriptiveError(`Plugin<${plugin.name}>.hooks.start() threw during loading`, e);
+            return false;
+        }
+    }
+
+    Plugins.push(plugin);
+    pluginNames.add(plugin.name);
+
+    logger.info(`Plugin<${plugin.name}> loaded`);
+
+    return true;
+}
+
+export function remove(plugin: Plugin): boolean {
+    if (!pluginNames.has(plugin.name)) {
+        logger.warn(`Plugin<${plugin.name}> removal failed because no such plugin could be found`);
+        return false;
+    }
+
+    const i = Plugins.indexOf(plugin);
+
+    if (i === -1) {
+        logger.error(new Error(`Plugin<${plugin.name}> passed to removePlugin() does not match loaded plugin object`));
+        return false;
+    }
+
+    pluginNames.delete(plugin.name);
+    Plugins.splice(i, 1)
+
+    if (plugin.hooks.stop) {
+        try { plugin.hooks.stop(); }
+        catch (e) { logger.descriptiveError(`Plugin<${plugin.name}>.hooks.stop() threw during removal`, e); }
+    }
+
+    return true;
+}
+
+
+process.on('exit', () => {
+    logger.info('Unloading all plugins before exit', true);
+    logger.addPreface('    ');
+    Plugins.forEach(plugin => {
+        try {
+            remove(plugin);
+        } catch (e) {
+            logger.error(`Domain<${plugin.name}> threw the following error during exit:` + Logger.parseError(e));
+        }
+    });
+});
