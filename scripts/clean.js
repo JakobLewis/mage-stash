a78026190803b6255a75db947d93a1c6a0bd981c
= <=>
2023-02-14 11:09:58 +0100

    clean.js now attempts to delete ./store/temp

--- scripts/clean.js
+++ scripts/clean.js
@@ -1,9 +1,14 @@
 // @ts-check
-import { rmSync, readdirSync } from "fs";
+import { rmSync, readdirSync, existsSync } from "fs";
 
 for (const filehandle of readdirSync('./lib', {withFileTypes: true})) {
     const path = './lib/' + filehandle.name;
     if (filehandle.isFile()) rmSync(path);
     else if (filehandle.isDirectory()) rmSync(path, { recursive: true, force: true });
 }
+
+
+if (existsSync('./store/temp'))
+    rmSync('./store/temp', {recursive: true, force: true});
+
 
