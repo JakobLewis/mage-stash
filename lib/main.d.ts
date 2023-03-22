fb0cd8f229c6481e657f7ee0c9f6d624f21213c1
= <=>
2023-03-22 18:37:26 +1100

    removed autoloading due to security concerns

--- lib/main.d.ts
+++ lib/main.d.ts
@@ -1,8 +1,7 @@
 import * as Logger from './logging.js';
-import * as Strings from './strings.js';
-import * as Wisp from './wisp.js';
-import * as Plugin from './plugin.js';
-import * as Handlers from './handler.js';
-import * as Library from './library.js';
-import Manifest from './manifest.js';
-export { Wisp, Strings, Plugin, Handlers, Library, Manifest, Logger };
+export * as Strings from './strings.js';
+export * as Wisp from './wisp.js';
+export * as Plugin from './plugin.js';
+export * as Library from './library.js';
+export * as Manifest from './manifest.js';
+export { Logger };
