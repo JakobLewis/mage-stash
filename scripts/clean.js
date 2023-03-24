21ecd4e344bb0ae5af1e67206f61d414b8a0e914
= <=>
2023-03-25 02:14:32 +1100

    clean script now removes entire ./lib folder

--- scripts/clean.js
+++ scripts/clean.js
@@ -1,9 +1,3 @@
 // @ts-check
-import { rmSync, readdirSync, existsSync } from "fs";
-
-readdirSync('./lib', {withFileTypes: true})
-    .forEach(dirent=>{
-        const path = './lib/' + dirent.name;
-        if (dirent.isFile()) rmSync(path);
-        else if (dirent.isDirectory()) rmSync(path, { recursive: true, force: true });
-    });
+import { rmSync } from "fs";
+rmSync('./lib', {force: true, recursive: true});
