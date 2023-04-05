d41608a9b706a581a71aa0fcbae96d7be8786763
= <=>
2023-04-06 08:18:08 +1000

    fixed small problems like poor grammar and shared Wisp references

--- src/manifest.test.ts
+++ src/manifest.test.ts
@@ -4,94 +4,144 @@ import { describe, it, expect } from '@jest/globals';
 
 loadDefaults();
 
-let contentWisp: Wisp.ContentWisp = {
+const contentWisp: Wisp.ContentWisp = {
     path: '/temporary',
     content: 'this is also content',
     metadata: {}
 };
 
-let groupWisp: Wisp.GroupWisp = {
+const groupWisp: Wisp.GroupWisp = {
     path: '/temp',
-    content: ['temp'],
+    content: ['temp', 'temporary'],
     metadata: {}
 };
 
-let childWisp: Wisp.ContentWisp = {
+const childWisp1: Wisp.ContentWisp = {
     path: '/temp/temp',
     content: 'this is also content',
     metadata: {}
 };
 
-
-const invalidWisp = {
-    path: '/',
-    content: 'this should fail',
+const childWisp2: Wisp.ContentWisp = {
+    path: '/temp/temporary',
+    content: 'this is also also content',
     metadata: {}
-} as const;
+};
+
 
 const success = Array.apply(null, Array(Manifest.count())).map(() => true);
+
+it('TODO: Unloading manifest plugin should cause all read/write attempts to fail', () => { });
+it('TODO: Access collisions should not occur', () => { });
 
 it('Calling loadDefaults() should mount at least 1 Manifest', () => assert(Manifest.count() > 0));
 
 describe('Illegal writes should fail', () => {
-    it('with a non-existent parent', async () => assert.notDeepEqual(await Manifest.writeWisp(childWisp), success));
-    it('with an invalid Wisp', async () => await expect(Manifest.writeWisp(invalidWisp)).rejects.toThrow(Error));
+    it('to the root path', async () => await expect(Manifest.writeWisp({
+        path: '/',
+        content: '',
+        metadata: {}
+    })).rejects.toThrow(Wisp.MalformedPathError));
+
+    it('to a path with invalid characters', async () => await expect(Manifest.writeWisp({
+        path: '/!@#$%^&*()_',
+        content: '',
+        metadata: {}
+    })).rejects.toThrow(Wisp.MalformedPathError));
+
+    it('to an empty parent path', async () => await expect(Manifest.writeWisp({
+        path: '//',
+        content: '',
+        metadata: {}
+    })).rejects.toThrow(Wisp.MalformedPathError));
+
+    it('with a missing parent', async () => assert.notDeepEqual(await Manifest.writeWisp(childWisp1), success));
 });
 
 describe('Deletes to nonexistent Wisps should fail', () => {
-    it('with a ContentWisp', async () => assert.notDeepEqual(await Manifest.deleteWisp(contentWisp.path), success));
-    it('with a GroupWisp', async () => assert.notDeepEqual(await Manifest.deleteWisp(groupWisp.path), success));
-    it('with a child ContentWisp', async () => assert.notDeepEqual(await Manifest.deleteWisp(childWisp.path), success));
+    it('ContentWisp', async () => assert.notDeepEqual(await Manifest.deleteWisp('/test'), success));
+    it('GroupWisp', async () => assert.notDeepEqual(await Manifest.deleteWisp(groupWisp.path), success));
+    it('child ContentWisps', async () => {
+        assert.notDeepEqual(await Manifest.deleteWisp(childWisp1.path), success);
+        assert.notDeepEqual(await Manifest.deleteWisp(childWisp2.path), success);
+    });
+});
+
+describe('Reads to nonexistent Wisps should fail', () => {
+    it('ContentWisp', async () => assert.equal(await Manifest.readWisp(contentWisp.path), undefined));
+    it('GroupWisp', async () => assert.equal(await Manifest.readWisp(groupWisp.path), undefined));
+    it('child ContentWisps', async () => {
+        assert.equal(await Manifest.readWisp(childWisp1.path), undefined);
+        assert.equal(await Manifest.readWisp(childWisp2.path), undefined);
+    });
 });
 
 describe('Valid writes should succeed', () => {
-    it('with a ContentWisp', async () => assert.deepEqual(await Manifest.writeWisp(contentWisp), success));
-    it('with a GroupWisp', async () => assert.deepEqual(await Manifest.writeWisp(groupWisp), success));
-    it('with a child ContentWisp', async () => assert.deepEqual(await Manifest.writeWisp(childWisp), success));
+    it('ContentWisp', async () => assert.deepEqual(await Manifest.writeWisp(contentWisp), success));
+    it('GroupWisp', async () => assert.deepEqual(await Manifest.writeWisp(groupWisp), success));
+    it('child ContentWisps', async () => {
+        assert.deepEqual(await Manifest.writeWisp(childWisp1), success);
+        assert.deepEqual(await Manifest.writeWisp(childWisp2), success);
+    });
 });
 
 describe('Valid reads should succeed', () => {
-    it('with a ContentWisp', async () => assert.deepEqual(await Manifest.readWisp(contentWisp.path), contentWisp));
-    it('with a GroupWisp', async () => assert.deepEqual(await Manifest.readWisp(groupWisp.path), groupWisp));
-    it('with a child ContentWisp', async () => assert.deepEqual(await Manifest.readWisp(childWisp.path), childWisp));
+    const gw = { ...groupWisp };
+    it('ContentWisp', async () => assert.deepEqual(await Manifest.readWisp(contentWisp.path), contentWisp));
+    it('GroupWisp', async () => assert.deepEqual(await Manifest.readWisp(groupWisp.path), gw));
+    it('ContentWisps', async () => {
+        assert.deepEqual(await Manifest.readWisp(childWisp1.path), childWisp1);
+        assert.deepEqual(await Manifest.readWisp(childWisp2.path), childWisp2);
+    });
 });
 
-contentWisp = {
+const newContentWisp = {
     ...contentWisp,
     content: 'less'
 };
 
-groupWisp = {
+const newGroupWisp = {
     ...groupWisp,
     metadata: {
         wowData: 1234
     }
 };
 
-childWisp = {
-    ...childWisp,
-    content: 'more more more way way way more content so much'
+const newChildWisp1 = {
+    ...childWisp1,
+    content: 'more more more way way way more content so much much wow'
 };
 
-describe('Overwrites should succeed', () => {
-    it('with a ContentWisp', async () => assert.deepEqual(await Manifest.writeWisp(contentWisp), success));
-    it('with a child ContentWisp', async () => assert.deepEqual(await Manifest.writeWisp(childWisp), success));
+describe('Overwrites of the same type should succeed', () => {
+    it('ContentWisp', async () => assert.deepEqual(await Manifest.writeWisp(newContentWisp), success));
+    it('GroupWisp', async () => assert.deepEqual(await Manifest.writeWisp(newGroupWisp), success));
+    it('child ContentWisp', async () => assert.deepEqual(await Manifest.writeWisp(newChildWisp1), success));
 });
 
 describe('Reads should reflect overwrites', () => {
-    it('with a ContentWisp', async () => assert.deepEqual(await Manifest.readWisp(contentWisp.path), contentWisp));
-    it('with a GroupWisp', async () => assert.deepEqual(await Manifest.readWisp(groupWisp.path), groupWisp));
-    it('with a child ContentWisp', async () => assert.deepEqual(await Manifest.readWisp(childWisp.path), childWisp));
+    it('ContentWisp', async () => assert.deepEqual(await Manifest.readWisp(newContentWisp.path), newContentWisp));
+    it('GroupWisp', async () => assert.deepEqual(await Manifest.readWisp(newGroupWisp.path), newGroupWisp));
+    it('child ContentWisps', async () => assert.deepEqual(await Manifest.readWisp(newChildWisp1.path), newChildWisp1));
+});
+
+const newNewGroupWisp = {
+    ...newGroupWisp,
+    content: ['temp']
+};
+
+describe('Deleting a child Wisp', () => {
+    it('should succeed', async () => assert.deepEqual(await Manifest.deleteWisp(childWisp2.path), success));
+    it('should fail to read after', async () => assert.equal(await Manifest.readWisp(childWisp2.path), undefined));
+    it('should be reflected in the parent GroupWisp', async () => assert.deepEqual(await Manifest.readWisp(newNewGroupWisp.path), newNewGroupWisp));
 });
 
 describe('Deletions should succeed', () => {
     it('with a ContentWisp', async () => assert.deepEqual(await Manifest.deleteWisp(contentWisp.path), success));
-    it('with a child ContentWisp', async () => assert.deepEqual(await Manifest.deleteWisp(childWisp.path), success));
     it('with a GroupWisp', async () => assert.deepEqual(await Manifest.deleteWisp(groupWisp.path), success));
 });
 
-it('GroupWisps should reflect removal and addition of children: TODO', () => { });
-it('Deleting a GroupWisp should delete child wisps: TODO', () => { });
-it('Access collisions should not occur: TODO', () => { });
-
-
+describe('After deletion, reads should fail', () => {
+    it('ContentWisp', async () => assert.equal(await Manifest.readWisp(contentWisp.path), undefined));
+    it('GroupWisp', async () => assert.equal(await Manifest.readWisp(groupWisp.path), undefined));
+    it('child GroupWisp', async () => assert.equal(await Manifest.readWisp(childWisp1.path), undefined));
+});
